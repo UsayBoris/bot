b1bf2687e98f73f33925025baebb43cb04b90f5f
UsayHi <rafaelsamorinha@gmail.com>
2021-01-10 21:07:23 +0000

    Mining command added with a fixed cooldown for now

--- commands/mine.js
+++ commands/mine.js
@@ -1,10 +1,24 @@
-//TODO mine for some coins, but with a cooldown https://stackoverflow.com/questions/48432102/discord-js-cooldown-for-a-command-for-each-user-not-all-users
+const talkedRecently = new Set();
+const {User} = require('../models/user');
 
 module.exports = {
     name: 'Mine',
-    description: 'Mine for some coins',
+    description: 'Mine for some coins (1 minute cooldown)',
     usage: 'mine',
     execute: async function (message, client, args) {
-
+        if(talkedRecently.has(message.author.id)){
+            message.reply('this command is still on cooldown for you');
+        }
+        else {
+            const bonus_coins = Math.floor(Math.random() * 5);
+            let user = await User.findOne({id: message.author.id});
+            user.coins += bonus_coins;
+            user.save();
+            message.reply(`you have mined ${bonus_coins}`);
+            talkedRecently.add(message.author.id);
+            setTimeout(() => {
+                talkedRecently.delete(message.author.id);
+            }, 60000);
+        }
     },
 };
