aa3fd9d626de32bb7270a7db266d8f476deb653c
UsayHi <rafaelsamorinha@gmail.com>
2021-01-11 18:37:17 +0000

    Changed Mongoose connection process

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const logger = require('../logger')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.on('error', () => logger.error("Can't Connect to MongoDB"));
-connection.once('open', () => logger.info("Connected to MongoDB database"));
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+    const connection = mongoose.connection;
+    connection.on('error', () => logger.error("Can't Connect to MongoDB"));
+    connection.once('open', () => logger.info("Connected to MongoDB database"));
+});
 module.exports = mongoose;
