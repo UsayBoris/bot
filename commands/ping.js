8efaa81f3ed5927dc8ea148d288ae63d859ddd15
UsayHi <rafaelsamorinha@gmail.com>
2021-01-22 22:49:46 +0000

    Fixed ping command displaying 0 latency!

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,6 @@ module.exports = {
     execute: async function (message, client, args) {
         //TODO create embed and forget about the edit thing
         await message.channel.send(new Discord.MessageEmbed()
-            .setDescription(`Pong! Latency is ${message.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`));
+            .setDescription(`Pong! Latency is ${message.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(client.ws.ping)}ms`));
     },
 };
