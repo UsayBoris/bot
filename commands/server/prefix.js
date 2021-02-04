f4c6228da07c6607c154a7f2c618201a678804b7
UsayHi <rafaelsamorinha@gmail.com>
2021-02-04 16:57:19 +0000

    Fixed prefix command bug

--- commands/server/prefix.js
+++ commands/server/prefix.js
@@ -5,13 +5,13 @@ module.exports = {
     description: 'Change the prefix for the bot in this server',
     usage: 'prefix {new prefix}',
     execute: async function (message, client, args) {
-        let new_prefix = args[0];
-
-        if (new_prefix === null)
-            return message.reply("you need to provide a valid prefix");
-
         if (!message.member.hasPermission('KICK_MEMBERS'))
             return message.reply("you don't have permissions to use this!");
+
+        if (!args.length)
+            return message.reply('you need to provide a valid new prefix')
+
+        let new_prefix = args[0];
 
         await Guild.findOneAndUpdate({id: message.guild.id}, {prefix: new_prefix}, {useFindAndModify: false})
             .then(() => {
