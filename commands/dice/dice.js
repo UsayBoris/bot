60b7c81911ab6896dd6f6d1715bf828aca354e71
UsayHi <rafaelsamorinha@gmail.com>
2021-06-26 23:54:23 +0100

    Dice Tests 1.0

--- commands/dice/dice.js
+++ commands/dice/dice.js
@@ -26,58 +26,60 @@ module.exports = {
 
             let roll_1 = Math.floor(Math.random() * 100) + 1;
 
-            let dice = new Discord.MessageEmbed()
+            let embedMessage = new Discord.MessageEmbed()
                 .setColor(0xAF873D)
                 .setTitle('Dice Challenge')
                 .setDescription(`You have challenged ${member.displayName} with a roll of ${roll_1}. Total value in the bet: ${args[1]} <:boriscoin:798017751842291732>`);
-
-            dice.react('✔').then(() => message.react('❌'));
 
             const filter = (reaction, user) => {
                 return ['✔', '❌'].includes(reaction.emoji.name) && user.id === member.id;
             }
 
-            dice.awaitReactions(filter, {max: 1, time: 60000, errors: ['time']})
-                .then(async collected => {
-                    const reaction = collected.first();
-                    switch (reaction.emoji.name) {
-                        case '✔':
-                            await User.findOne({id: member.id}).then(async user2 => {
-                                if (user2.coins < args[1]) {
-                                    dice.setDescription('You dont have enough coins to accept this challenge. Cancelled!');
-                                    await dice.reactions.removeAll();
-                                }
+            message.channel.send(embedMessage).then(async dice => {
+                await embedMessage.react('✔').then(() => message.react('❌'));
 
-                                let roll_2 = Math.floor(Math.random() * 100) + 1;
+                dice.awaitReactions(filter, {max: 1, time: 60000, errors: ['time']})
+                    .then(async collected => {
+                        const reaction = collected.first();
+                        switch (reaction.emoji.name) {
+                            case '✔':
+                                await User.findOne({id: member.id}).then(async user2 => {
+                                    if (user2.coins < args[1]) {
+                                        dice.edit.setDescription('You dont have enough coins to accept this challenge. Cancelled!');
+                                        await dice.reactions.removeAll();
+                                    }
 
-                                if (roll_2 > roll_1) {
-                                    user1.coins -= args[1];
-                                    await user1.save();
-                                    user2.coins += args[1];
-                                    await user2.save();
+                                    let roll_2 = Math.floor(Math.random() * 100) + 1;
 
-                                    dice.setDescription(`${member.displayName} won the dice with a roll of ${roll_2} vs ${roll_1}, and received ${args[1]} <:boriscoin:798017751842291732>`);
-                                    await dice.reactions.removeAll();
-                                }
-                                else if(roll_1 >= roll_2){
-                                    user1.coins += args[1];
-                                    await user1.save();
-                                    user2.coins -= args[1];
-                                    await user2.save();
+                                    if (roll_2 > roll_1) {
+                                        user1.coins -= args[1];
+                                        await user1.save();
+                                        user2.coins += args[1];
+                                        await user2.save();
 
-                                    dice.setDescription(`${member.displayName} lost the dice with a roll of ${roll_2} vs ${roll_1}, he loses ${args[1]} <:boriscoin:798017751842291732>`);
-                                    await dice.reactions.removeAll();
-                                }
+                                        dice.edit.setDescription(`${member.displayName} won the dice with a roll of ${roll_2} vs ${roll_1}, and received ${args[1]} <:boriscoin:798017751842291732>`);
+                                        await dice.reactions.removeAll();
+                                    } else if (roll_1 >= roll_2) {
+                                        user1.coins += args[1];
+                                        await user1.save();
+                                        user2.coins -= args[1];
+                                        await user2.save();
 
-                            })
-                            break;
-                        case '❌':
-                            dice.setDescription(`${member.displayName} declined the dice, better friends next time!`);
-                            await dice.reactions.removeAll();
-                            break;
-                    }
-                });
-            await message.channel.send(dice);
+                                        dice.edit.setDescription(`${member.displayName} lost the dice with a roll of ${roll_2} vs ${roll_1}, he loses ${args[1]} <:boriscoin:798017751842291732>`);
+                                        await dice.reactions.removeAll();
+                                    }
+
+                                })
+                                break;
+                            case '❌':
+                                dice.edit.setDescription(`${member.displayName} declined the dice, better friends next time!`);
+                                await dice.reactions.removeAll();
+                                break;
+                        }
+                    });
+            });
+
+
         });
     }
 }
