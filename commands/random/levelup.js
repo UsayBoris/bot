18dd6a03da0f2a7fc639dd456771e12299f2b707
UsayHi <rafaelsamorinha@gmail.com>
2021-06-27 23:57:01 +0100

    Levelup command now sends to the channel, not to the user

--- commands/random/levelup.js
+++ commands/random/levelup.js
@@ -10,7 +10,7 @@ module.exports = {
 
             let req_xp = 69 * (user.level + 1) * (1 + (user.level + 1));
 
-            return message.author.send(new Discord.MessageEmbed()
+            return message.channel.send(new Discord.MessageEmbed()
                 .setColor("0xACA19D")
                 .setTitle("You're almost there")
                 .setDescription(`You still need ${req_xp - user.xp} to reach level ${user.level+1}. Keep spamming!`));
