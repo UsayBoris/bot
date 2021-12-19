db559b1299b23d0a2ac6a915bcc1c73146a00a6c
UsayHi <rafaelsamorinha@gmail.com>
2021-12-19 23:15:19 +0000

    fixed error on index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const ElBoris = require("./struct/Client");
+const client = new ElBoris();
+const {commandHandler} = require("./commands");
+const {update_user} = require('./models/user');
+const Guild = require('./models/guild');
+const logger = require('./logger');
+
+//TODO pretty embed for everything that needs an embed
+
+//Bot startup message
+client.on('ready', async () => {
+    logger.info(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds`)
+    await client.user.setPresence({
+        activity: {
+            name: '+help',
+            type: 'LISTENING'
+        },
+        status: 'online'
+    });
+});
+
+//When the bot is added to a new server
+client.on('guildCreate', async guild => {
+    logger.info(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`)
+    await Guild.create({name: guild.name, id: guild.id});
+});
+
+//When the bot is removed from a server
+client.on("guildDelete", guild => {
+    logger.info(`I have been removed from: ${guild.name} (id: ${guild.id})`)
+});
+
+//Command handler
+client.on('message', async message => {
+    if (message.author.bot) return;
+
+    if (message.content === '@everyone') {
+        return message.reply('@everyone ping ping @everyone');
+    }
+
+    await update_user(message);
+
+    const guild = await Guild.findOne({id: message.guild.id});  //the guild should be created when it joins, so no fail check here
+    const prefix = guild['prefix'];
+
+    if (!message.content.startsWith(prefix)) return;
+
+    await commandHandler(message, client, prefix);              //very well made command handler :)
+
+    logger.command(`User ${message.author.username} send a command to ${message.channel.name} in ${message.guild.name}`);
+});
+
+//new member added
+client.on('guildMemberAdd', async member => {
+    logger.info(`New User ${member.user.username} has joined ${member.guild.name}`);
+    await member.guild.channels.cache.findOne(c => c.name === "welcome").send(`${member.user.username} has joined this server`);
+});
+
+client.on('error', e => logger.error(e));
+client.on('warn', e => logger.warn(e));
+client.on('debug', e => logger.debug(e));
+
+client.login(process.env.DISCORD_API).then();
